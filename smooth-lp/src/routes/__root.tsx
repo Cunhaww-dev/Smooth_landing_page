216dd573453b6def2d2e562a1fadd4a6bc351726
Cunhaww-dev <dev.cunhaww@gmail.com>
2025-11-03 09:55:06 -0300

    feat: use footer on root layout

--- smooth-lp/src/routes/__root.tsx
+++ smooth-lp/src/routes/__root.tsx
@@ -1,11 +1,13 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
-import Header from "../components/header";
+import { createRootRoute, Outlet } from '@tanstack/react-router'
+import Header from '../components/header'
+import Footer from '../components/footer'
 
 const RootLayout = () => (
   <>
     <Header />
     <Outlet />
+    <Footer />
   </>
-);
+)
 
-export const Route = createRootRoute({ component: RootLayout });
+export const Route = createRootRoute({ component: RootLayout })
